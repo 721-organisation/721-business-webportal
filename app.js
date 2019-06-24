5e707a916c35ce58d168d2c25682ccac6e51cf5a
Alex Yates <adyates1@sheffield.ac.uk>
2019-06-24 14:29:40 +0100

    Added functionality to add hidden gems

--- app.js
+++ app.js
@@ -107,6 +107,66 @@ io.on('connection', function (socket) {
             }
         });
     });
+    socket.on('newHiddenGemSubmitted', function (data) {
+        var hiddenGemData = data.event;
+        var venueAddress = data.address;
+        getLatLongFromAddress(venueAddress, function (latLong) {
+            hiddenGemData.venueLat = latLong.latitude;
+            hiddenGemData.venueLong = latLong.longitude;
+            var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+            var string_length = 16;
+            var randomstring = '';
+            for (var i=0; i<string_length; i++) {
+                var rnum = Math.floor(Math.random() * chars.length);
+                randomstring += chars.substring(rnum,rnum+1);
+            }
+            hiddenGemData.eventSourceId = randomstring;
+            hiddenGemData.eventSourceTag = "BUSINESS";
+            console.log("test");
+            try {
+                var request = require("request");
+
+                var options = {
+                    method: 'POST',
+                    url: 'https://temp-243314.appspot.com/api/Users/login',
+                    headers:
+                        {
+                            'cache-control': 'no-cache',
+                            Connection: 'keep-alive',
+                            'accept-encoding': 'gzip, deflate',
+                            Accept: '*/*',
+                            'Content-Type': 'application/json'
+                        },
+                    body: {email: userDetails.email, password: userDetails.password},
+                    json: true
+                };
+
+                request(options, function (error, response, body) {
+                    if (error) throw new Error(error);
+                    var accessToken = body.id;
+                    var settings = { method: 'POST',
+                        url: 'https://temp-243314.appspot.com/api/requestedHiddenGem',
+                        qs: { access_token: accessToken },
+                        headers:
+                            { 'cache-control': 'no-cache',
+                                Connection: 'keep-alive',
+                                'accept-encoding': 'gzip, deflate',
+                                Accept: '*/*',
+                                'Content-Type': 'application/json' },
+                        body: hiddenGemData,
+                        json: true };
+
+                    request(settings, function (error, response, b)  {
+                        if (error) throw new Error(error);
+                        console.log(b);
+                    });
+                });
+
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    });
 });
 
 http.listen(80, function () {
